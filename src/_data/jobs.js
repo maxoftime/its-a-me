262ac5a4a67903ea104edd76d2bc1cf334b52f10
Max Bergström <max.bergstrom@familjen.se>
2022-10-10 14:49:25 +0200

    feat: added skills and some work desc

---
+++ src/_data/jobs.js
@@ -0,0 +1,40 @@
+const work = [
+    {
+        client: "Kia Sweden",
+        role: "Frontend developer",
+        description: "My role at Kia was to add new pages and also maintain existing pages in their Adobe Experience Manager CMS. It involved a need for smartness and working around larger issues that was was inherent in the CMS. It also involved developing standalone campaign sites in HTML, CSS and JS. I had close contact with their tech lead and also functioned as an advisory in strategy in the work on their site.",
+        stack: ["HTML", "CSS", "JS", "AEM"]
+    },
+    {
+        client: "Sunt Arbetsliv",
+        role: "Fullstack developer",
+        description: "I was responsible for developing a new site under Sunt Arbetslivs multisite umbrella in Wordpress. I started with an existing site and migrated it to a new one, while also implementing new design, new features and also developing new components for the block builder WP Bakery.",
+        stack: ["HTML", "PHP", "Wordpress", "CSS", "SASS", "MySQL"]
+    },
+    {
+        client: "Specialpedagogiska Skolmyndigheten",
+        role: "CSS hacker",
+        description: "By just using the inbuilt developer tools in the browser, I was consulting the main developers in implementing a new design on SPSMs current fleet of sites.",
+        stack: ["CSS", "browser devTools"]
+    },
+    {
+        client: "Familjen STHLM",
+        role: "Frontend Developer",
+        description: "The project involved structuring and frontend coding our new site. The main goal was to showcase the playfulness of the web while at the same time deliver a fast and nice experience to the visitor.",
+        stack: ["HTML", "CSS", "JS", "Eleventy", "SanityCMS"]
+    },
+    {
+        client: "NTM",
+        role: "Internal fullstack developer with frontend focus",
+        description: "At NTM, We had the need to build our own internal tools to automate time consuming and repetetive tasks and also be able to present data in a understandable manner for our sales reps and our ad buyers. It was a mix of internal python scripts, websites, APIs/webservices and a larger React project with typescript.",
+        stack: ["Azure", "Node", "React", "Typescript", "Git", "MSSQL", "REST", "SOAP", "CSS", "HTML", "Python"]
+    },
+    {
+        client: "Stjärnkalaset - Teatercentrum",
+        role: "Fullstack developer",
+        description: "Maintain content and develop new features with focus on accessibility and on their existing wordpress platform",
+        stack: ["HTML", "PHP", "Wordpress", "CSS", "MySQL"]
+    }
+];
+
+module.exports = work;
---
+++ src/_data/skills.js
@@ -0,0 +1,161 @@
+const skills = [
+    {
+        name: "HTML",
+        platform: "web",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "CSS",
+        platform: "web",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "Javascript",
+        platform: "Web",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "Node",
+        platform: "Javascript",
+        type: "Serverside",
+        rating: ""
+    },
+    {
+        name: "Typescript",
+        platform: "Javascript",
+        type: "Serverside",
+        rating: ""
+    },
+    {
+        name: "Accessibility",
+        platform: "Web",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "React",
+        platform: "Javascript",
+        type: "Serverside/Clientside",
+        rating: ""
+    },
+    {
+        name: "Vue",
+        platform: "Javascript",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "Jquery",
+        platform: "Javascript",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "Eleventy",
+        platform: "Javascript",
+        type: "Static site generator",
+        rating: ""
+    },
+    {
+        name: "PHP",
+        platform: "php",
+        type: "Serverside",
+        rating: ""
+    },
+    {
+        name: "Sass",
+        platform: "CSS",
+        type: "Serverside",
+        rating: ""
+    },
+    {
+        name: "Bootstrap",
+        platform: "CSS",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "Python",
+        platform: "python",
+        type: "Serverside",
+        rating: ""
+    },
+    {
+        name: "Git",
+        platform: "",
+        type: "Version control system",
+        rating: ""
+    },
+    {
+        name: "Azure Devops",
+        platform: "Azure",
+        type: "Devops",
+        rating: ""
+    },
+    {
+        name: "MySQL",
+        platform: "Web",
+        type: "Backend",
+        rating: ""
+    },
+    {
+        name: "MSSQL",
+        platform: "Azure",
+        type: "Backend",
+        rating: ""
+    },
+    {
+        name: "REST",
+        platform: "Web",
+        type: "API",
+        rating: ""
+    },
+    {
+        name: "SOAP",
+        platform: "Web",
+        type: "API",
+        rating: ""
+    },
+    {
+        name: "Responsive design",
+        platform: "Web",
+        type: "Clientside",
+        rating: ""
+    },
+    {
+        name: "UX",
+        platform: "UI",
+        type: "Design",
+        rating: ""
+    },
+    {
+        name: "Graphic design",
+        platform: "",
+        type: "Design",
+        rating: ""
+    },
+    {
+        name: "Wordpress",
+        platform: "CMS",
+        type: "CMS",
+        rating: ""
+    },
+    {
+        name: "Sanity",
+        platform: "CMS",
+        type: "CMS",
+        rating: ""
+    },
+    {
+        name: "Contentful",
+        platform: "CMS",
+        type: "CMS",
+        rating: ""
+    },
+    
+];
+
+module.exports = skills;

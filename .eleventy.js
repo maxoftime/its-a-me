c5a111b8c0e59736a02fe5e1fe5e796f821068bc
Max Bergström <max.bergstrom@familjen.se>
2022-10-12 09:15:36 +0200

    feat: start of blog function + max-width on skills cloud

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({"src/_includes/assets/" : "assets/" });
+    eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+    
+    eleventyConfig.addFilter('dateIso', date => {
+      return date.toISOString();
+    });
+  
+    eleventyConfig.addFilter('dateReadable', date => {
+      return date.toLocaleDateString();
+    });
 
     return {
         dir: {
@@ -9,3 +18,31 @@ module.exports = function(eleventyConfig) {
         "dataTemplateEngine": "njk"
     }
 }
+
+function extractExcerpt(article) {
+    if (!article.hasOwnProperty('templateContent')) {
+      console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+      return null;
+    }
+   
+    let excerpt = null;
+    const content = article.templateContent;
+   
+    // The start and end separators to try and match to extract the excerpt
+    const separatorsList = [
+      { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+      { start: '<p>', end: '</p>' }
+    ];
+   
+    separatorsList.some(separators => {
+      const startPosition = content.indexOf(separators.start);
+      const endPosition = content.indexOf(separators.end);
+   
+      if (startPosition !== -1 && endPosition !== -1) {
+        excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+        return true; // Exit out of array loop on first match
+      }
+    });
+   
+    return excerpt;
+  }
---
+++ src/blog/2022-10-11/how-to-get-rich-quick.md
@@ -0,0 +1,16 @@
+---
+layout: layouts/post.njk 
+title: How To Get Rich Quick
+date: 2022-10-11
+tags: ['post']
+---
+<!-- Excerpt Start -->
+This is included on the homepage.
+<!-- Excerpt End -->
+
+kjdfskdjbfjk sdkjdsf kjdsf ksdkj  kds¨
+sd
+fsd
+fsd
+f
+sd

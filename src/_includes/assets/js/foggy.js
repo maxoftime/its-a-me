15942f0e02eacefe4b9b1db64cb7dca6975d5f29
Max Bergström <maxoftime@gmail.com>
2023-10-26 16:00:57 +0200

    new design and updates to cases

--- src/_includes/assets/js/foggy.js
+++ src/_includes/assets/js/foggy.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { ImprovedNoise } from 'three/addons/math/ImprovedNoise.js';
+import { Sky } from 'three/addons/objects/Sky.js';
 
 // Setup
 const scrollDistance = { x: 0, y: 0 }
@@ -14,67 +14,118 @@ const renderer = new THREE.WebGLRenderer({
 // Init
 camera.position.z = 5;
 camera.position.y = 2;
-camera.lookAt = (0,1,0);
+//camera.lookAt = (0,1.5,0);
 
+renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1)
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+
 renderer.domElement.classList.add('canvas-background');
 document.body.appendChild( renderer.domElement );
 
 
 
 // Content
-/* const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 2   );
-//hemiLight.color.setHSL( 0.6, 1, 0.6 );
-//hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-hemiLight.position.set( 0, 100, 0 );
-hemiLight.castShadow = true;
-scene.add( hemiLight ); */
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 
+const sky = new Sky();
+sky.scale.setScalar( 10 );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshStandardMaterial( { 
-    transparent: true,
-    color: 0x00ff00,
-    opacity: 0.5
-} );
-const cube = new THREE.Mesh( geometry, material );
-cube.castShadow = true;
-cube.receiveShadow = true;
-//scene.add( cube );
+const skyUniforms = sky.material.uniforms;
+
+skyUniforms[ 'turbidity' ].value = 0;
+skyUniforms[ 'rayleigh' ].value = 0.01;
+skyUniforms[ 'mieCoefficient' ].value = 0.005;
+skyUniforms[ 'mieDirectionalG' ].value = 0.7;
+
+const parameters = {
+    rayleigh: 0,
+    elevation: 2.25,
+    azimuth: 198.77,
+    turbidity: 2.3,
+    exposure: 0.3,
+    mieCoefficient: 0.005,
+	mieDirectionalG: 0.5,
+};
+
+/* let sun = new THREE.Vector3(); const phi = THREE.MathUtils.degToRad( 90 - parameters.elevation );
+const theta = THREE.MathUtils.degToRad( parameters.azimuth );
+sun.setFromSphericalCoords( 0.01, phi, theta );
+sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+scene.add( sky );  */
+
 
 const textureLoader = new THREE.TextureLoader()
-//const groundTextureColor = textureLoader.load('/assets/img/textures/ground/baseColor.jpg')
+const groundTextureColor = textureLoader.load('/assets/img/textures/ground/baseColor.jpg')
 const groundTextureHeight = textureLoader.load('/assets/img/textures/ground/height.png')
-//const groundTextureRough = textureLoader.load('/assets/img/textures/ground/rough.jpg')
+groundTextureHeight.wrapS = THREE.RepeatWrapping;
+groundTextureHeight.wrapT = THREE.RepeatWrapping;
+//groundTextureHeight.repeat.set( 4, 4 );
 
-const groundGeo = new THREE.PlaneGeometry( 40, 10, 300, 160 );
+
+const groundGeo = new THREE.PlaneGeometry( 40, 10, 100, 360 );
 const groundMat = new THREE.MeshStandardMaterial( {
-    //map: groundTextureColor,
     displacementMap: groundTextureHeight,
-    //roughness: groundTextureRough,
+    displacementScale: 2,
+    //roughness: groundTextureHeight,
     wireframe: true,
-    color: 0x4444444,
+    emissive: 0x333333,
+    //emissiveMap: groundTextureColor,
     transparent: true,
     opacity: 0.5
 } );
 const ground = new THREE.Mesh( groundGeo, groundMat );
 ground.rotation.x = -1.3962634;
-ground.position.y = -1;
-cube.castShadow = true;
-cube.receiveShadow = true;
+ground.position.y = -2.5;
 scene.add( ground );
+
+const leftRockGeo = new THREE.BoxGeometry( 0.7, 8, 0.2 );
+const leftRockMat = new THREE.MeshBasicMaterial( { 
+    transparent: true,
+    color: 0xaa0000,
+    //opacity: 0.5
+} );
+const leftRock = new THREE.Mesh( leftRockGeo, leftRockMat );
+leftRock.position.set(-7, 3.1, -4)
+//scene.add( leftRock );
+
+
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
 
 
 // Render
 function render() {
-    requestAnimationFrame(render);
-
+    
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+    
     if (scrollDistance.y !== window.scrollY) {
-        scrollDistance.y = window.scrollY;
-        cube.rotation.y = scrollDistance.y * 0.005;
-       camera.position.y = 2 - scrollDistance.y * 0.005;
+        //if (window.scrollY < 460) {
+            scrollDistance.y = window.scrollY;
+            //leftRock.rotation.y = scrollDistance.y * 0.005;
+            camera.position.y = 2 - scrollDistance.y * 0.005;
+            //groundTextureHeight.offset = new THREE.Vector2(0, scrollDistance.y / window.innerHeight * 0.1)
+
+       // }
     }
     renderer.render( scene, camera );
+
+    requestAnimationFrame(render);
 }
 
 render();
